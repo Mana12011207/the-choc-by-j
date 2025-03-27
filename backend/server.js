4a25fd7714da9c12f556ec11805ee0d7ca1381b6
Mana Misumi <mana.misumi3050@gmail.com>
2025-03-28 09:06:56 +1100

    Established connection to MongoDB Atlas using Mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,40 @@
-import express from "express";
-import mongoose from "mongoose";
+import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+import { MongoClient, ServerApiVersion } from "mongodb";
 
-const app = express();
-const PORT = process.env.PORT || 5000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-mongoose.connect();
+dotenv.config({ path: path.resolve(__dirname, "../.env") });
 
-app.length("/", (req, res) => {
-  res.send("Hello from backend");
+console.log("Environment variables loaded:", process.env.DB_PASSWORD);
+
+const dbURI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@thechocbyj-dev.etc1o88.mongodb.net/?retryWrites=true&w=majority&appName=TheChocByJ-Dev`;
+
+console.log("Datebase URI:", dbURI);
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(dbURI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port{PORT}`);
-});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.error);
