676440a4a4fe907c0c43edfba12d706415ad596c
Mana Misumi <mana.misumi3050@gmail.com>
2025-04-06 07:54:51 +1000

    Delete and update files

--- backend/server.js
+++ backend/server.js
@@ -2,17 +2,15 @@ import dotenv from "dotenv";
 import path from "path";
 import { fileURLToPath } from "url";
 import { MongoClient, ServerApiVersion } from "mongodb";
+import bodyParser from "body-parser";
 
+// Configureation for enviroment variables
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 dotenv.config({ path: path.resolve(__dirname, "../.env") });
 
-console.log("Environment variables loaded:", process.env.DB_PASSWORD);
-
 const dbURI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@thechocbyj-dev.etc1o88.mongodb.net/?retryWrites=true&w=majority&appName=TheChocByJ-Dev`;
-
-console.log("Datebase URI:", dbURI);
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(dbURI, {
@@ -23,18 +21,27 @@ const client = new MongoClient(dbURI, {
   },
 });
 
-async function run() {
+const app = express();
+app.use(bodyParser.json());
+
+let productsCollection;
+
+(async function connectDB() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+    // Connect the client to the server
     await client.connect();
+    console.log("You successfully connected to MongoDB.");
+
+    const database = client.db("thechocbyj");
+    productsCollection = database.collection("products");
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+    console.log("Ping successful: MongoDB is responding.");
+  } catch (err) {
+    console.error("Database connection error:", err);
+    process.exsit(1);
   }
-}
-run().catch(console.error);
+})();
+
+//
