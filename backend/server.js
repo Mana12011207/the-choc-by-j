2944dd7dd8adb98a29c495ff14eb406903c9bf18
Mana Misumi <mana.misumi3050@gmail.com>
2025-04-07 09:46:10 +1000

    Create index.js and add cros options to allow requests only from the client host

--- backend/server.js
+++ backend/server.js
@@ -1,47 +1,32 @@
-import dotenv from "dotenv";
-import path from "path";
-import { fileURLToPath } from "url";
-import { MongoClient, ServerApiVersion } from "mongodb";
-import bodyParser from "body-parser";
+import express from "express";
+import productRoutes from "./routes/productRoutes";
+import userRoutes from "./routes/userRoutes";
+import categoryRoutes from "./routes/categoryRoutes";
+import orderRoutes from "./routes/orderRoutes";
+import cors from "cors";
 
-// Configureation for enviroment variables
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-dotenv.config({ path: path.resolve(__dirname, "../.env") });
-
-const dbURI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@thechocbyj-dev.etc1o88.mongodb.net/?retryWrites=true&w=majority&appName=TheChocByJ-Dev`;
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(dbURI, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-});
-
+//Define a server instance
 const app = express();
-app.use(bodyParser.json());
 
-let productsCollection;
+// Middleware to enable request.body
+app.use(express.json());
 
-(async function connectDB() {
-  try {
-    // Connect the client to the server
-    await client.connect();
-    console.log("You successfully connected to MongoDB.");
+// Middleware to parse the URL-encoded data
+app.use(express.urlencoded({ extended: true }));
 
-    const database = client.db("thechocbyj");
-    productsCollection = database.collection("products");
+// Set CORS options to allow requests only from the client host specfied in the environment configuration.
+const corsOptions = {
+  origin: process.env.CLIENT_HOST || "http:localhost:3000",
+  optionSuccessStatus: 200,
+};
 
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Ping successful: MongoDB is responding.");
-  } catch (err) {
-    console.error("Database connection error:", err);
-    process.exsit(1);
-  }
-})();
+// Enable CORS - Cross Origin Resource Sharing
+app.use(cors(corsOptions));
 
-//
+//Routes
+app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/categories", categoryRoutes);
+app.use("/api/orsers", orderRoutes);
+
+export default app;
